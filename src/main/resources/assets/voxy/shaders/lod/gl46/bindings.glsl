3e2dc1bea99e5d4e5ad31cd64eeaaf563c835feb
DebuNeko233 <101854723+DebuNeko233@users.noreply.github.com>
2026-09-12 19:33:14 +0800

    Carry native Voxy render distance in scene uniform

--- src/main/resources/assets/voxy/shaders/lod/gl46/bindings.glsl
+++ src/main/resources/assets/voxy/shaders/lod/gl46/bindings.glsl
@@ -3,6 +3,9 @@ layout(binding = 0, std140) uniform SceneUniform {
     ivec3 baseSectionPos;
     uint frameId;
     vec3 cameraSubPos;
+    // Matches the classic Iris/Voxy vxRenderDistance uniform. Photon converts
+    // this chunk count to blocks by multiplying it by 16.
+    float vxRenderDistance;
 };
 
 //TODO: see if making the stride 2*4*4 bytes or something cause you get that 16 byte write
